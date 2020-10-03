e047ecce4d2e1c8f829ccc60228ba75a3b45515c
JohnDavid <jhndagon@gmail.com>
2020-10-02 19:22:02 -0500

    añadido deteccion de color y movimiento con camara

---
+++ js/camera.js
@@ -0,0 +1,116 @@
+let camera = document.getElementById('camera')
+
+function errorCamera(error){
+    console.error(error)
+}
+
+function activateCamera(message) {
+    camera.srcObject = message
+    camera.height = 250;
+    camera.width = 250;
+    camera.play()
+}
+
+navigator.getUserMedia({video: true}, activateCamera, errorCamera)
+
+let canvas = document.getElementById('videoCanvas')
+let context = canvas.getContext('2d')
+canvas.height = 250;
+canvas.width = 250;
+context.height = canvas.height;
+context.width = canvas.width;
+
+let src = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4)
+// let dest = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC1)
+let dest2 = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC1)
+
+// let greenBajo = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4, [50,100,20,0])
+// let greenAlto = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4, [60,255,255,0])
+
+
+let low = new cv.Mat(250, 250, 16, [50, 100, 20, 255]);
+let high = new cv.Mat(250, 250, 16, [60, 255, 255, 255]);
+
+
+let mask = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4)
+const FPS = 60;
+let flag = true;
+let contours = new cv.MatVector();
+let hierarchy= new cv.Mat();
+let begin = Date.now()
+let area = 0;
+function processVideo() {
+    
+    context.drawImage(camera, 0, 0, canvas.width, canvas.height)
+    src.data.set(context.getImageData(0, 0, canvas.width, canvas.height).data);
+    cv.cvtColor(src, dest2, cv.COLOR_RGB2HSV, 0)
+    // console.log(dest2);
+
+    cv.inRange(dest2, low, high, mask)
+    // cv.bitwise_and(src, src, dest2, mask)
+    // cv.cvtColor(src, dest, cv.COLOR_RGBA2GRAY);
+    
+
+    cv.findContours(mask, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+
+    for(let i  = 0; i < contours.size(); ++i) {
+        let cnt = contours.get(i)
+        area = cv.contourArea(cnt, false)
+        if(area > 10000) {
+            // let keyEvent = new KeyboardEvent("eventKeyPress", {"keyCode": 32})
+            // console.log(keyEvent)
+            // let game = document.getElementById('game-canvas').dispatchEvent(keyEvent)
+            // console.log(area, i)
+            console.log("lanzar golpe")
+            console.log(area)
+            scorpio_space_press()
+        }
+        else if(area >1500) {
+            M = cv.moments(cnt, false);
+            if(M.m00==0){
+                M.m00=1
+            }
+            x = M.m10/M.m00
+            y = M.m01/M.m00
+            x = Math.floor(x)
+            y = Math.floor(y)
+            // console.log(x,y)
+ 
+            if(x > 180){
+                scorpio_right_press()
+                console.log(x)
+            }
+            else if(x < 120) {
+                scorpio_left_press()
+            }
+            else {
+                scorpio_right_release()
+                scorpio_left_release()
+            }
+            
+
+            let center = new cv.Point(x,y)
+            let col = new cv.Scalar(0, 255, 0)
+            cv.circle(src, center, 7, col)
+            let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
+            Math.round(Math.random() * 255));
+            cv.drawContours(src, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+
+        }
+        area = 0;
+    }
+    cv.imshow("videoCanvas", src)
+
+    if(flag) {
+        flag = false
+        console.log(contours)
+    }
+
+    // socket.emit('videoStream', {id: socket.id, data: canvas.toDataURL('image/webp')})
+
+    let delay = 1000/FPS - (Date.now() - begin);
+    // console.log((Date.now() - begin)/1000)
+    // setTimeout(processVideo, delay);
+}
+
+setInterval(processVideo, 60)
